932dd83f425366096d8bd509e6bdc7e91d7a30e9
ThuongDoo <domanhthuong20122002@gmail.com>
2025-08-12 22:18:59 +0700

    favicon

--- components/ProjectsBtn2.jsx
+++ components/ProjectsBtn2.jsx
@@ -8,7 +8,7 @@ const ProjectsBtn2 = () => {
   return (
     <div className=" mx-auto xl:mx-0">
       <a
-        href="https://mucintrungtin.com/" // link ngoài
+        href="https://drive.google.com/file/d/11BxN83igSDFsTsSvZ_FTSV9nABbwyFUR/view?usp=sharing" // link ngoài
         target="_blank"
         rel="noopener noreferrer"
         className="relative w-[185px] h-[185px] flex justify-center items-center z-[5] bg-[url('https://raw.githubusercontent.com/ThuongDoo/images/cc1a2485fd27398ae6511f8a16dbcfafcf03d116/circle-star.svg')] bg-cover bg-center bg-no-repeat group"
--- pages/_app.js
+++ pages/_app.js
@@ -7,20 +7,53 @@ import Transition from "../components/Transition";
 // router
 import { useRouter } from "next/router";
 
-// frame motion
+// framer motion
 import { AnimatePresence, motion } from "framer-motion";
+
+// Head
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+
   return (
-    <Layout>
-      <AnimatePresence mode="wait">
-        <motion.div key={router.route} className="h-full">
-          <Transition />
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-    </Layout>
+    <>
+      <Head>
+        <title>Đỗ Mạnh Thường - Dịch Vụ Web & IT</title>
+
+        <meta
+          name="description"
+          content="Portfolio và dịch vụ lập trình web của Đỗ Mạnh Thường"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Đỗ Mạnh Thường - Dịch Vụ Web & IT"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+      </Head>
+
+      <Layout>
+        <AnimatePresence mode="wait">
+          <motion.div key={router.route} className="h-full">
+            <Transition />
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+      </Layout>
+    </>
   );
 }
 
